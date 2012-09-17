aa932061e5ccc35677521a8d212387911c9cd98b
Tom Nason <tdn10@pitt.edu>
2012-09-17 16:28:01 -0400

    Adding Client/Server test
    Will update to non-blocking (event driven keypress) input

---
+++ PodSixNet-test/ClientTest.py
@@ -0,0 +1,51 @@
+import sys
+from time import sleep
+from sys import stdin, exit
+
+from PodSixNet.Connection import connection, ConnectionListener
+
+#do i need to thread like they did?
+#Yeah, the stdin call is blocking, so I do.
+#TODO: Change this from the loop structure to polling for key press events
+from thread import *
+
+class Client(ConnectionListener):
+    def __init__(self, host, port):
+            self.Connect((host, port))
+            print "Chat client started"
+            #"Bad" threaded input loop
+            t = start_new_thread(self.InputLoop, ())
+
+    def Loop(self):
+            connection.Pump()
+            self.Pump()
+
+    def InputLoop(self):
+            while 1:
+                connection.Send({"action": "packet", "message": stdin.readline().rstrip("\n")})
+
+    #Network events
+    def Network_packetConfirm(self, data):
+        print "Packet confirmation received." + str(data['packet'])
+
+    #built in Network events
+    def Network_connected(self, data):
+    	print "You are now connected to the server"
+	
+    def Network_error(self, data):
+	print 'error:', data['error'][1]
+	connection.Close()
+	
+    def Network_disconnected(self, data):
+	print 'Server disconnected'
+	exit()
+
+if len(sys.argv) != 2:
+	print "Usage:", sys.argv[0], "host:port"
+	print "e.g.", sys.argv[0], "localhost:31425"
+else:
+	host, port = sys.argv[1].split(":")
+	c = Client(host, int(port))
+	while 1:
+		c.Loop()
+		sleep(0.001)
---
+++ PodSixNet-test/ServerTest.py
@@ -0,0 +1,59 @@
+#Server example for sending data over the network
+import sys
+from time import sleep
+from weakref import WeakKeyDictionary
+
+from PodSixNet.Server import Server
+from PodSixNet.Channel import Channel
+
+#Server representation of a connected client
+class ClientChannel(Channel):
+        def __init__(self, *args, **kwargs):
+            Channel.__init__(self, *args, **kwargs)
+
+        def Close(self):
+            self._server.DelClient(self)
+
+        # Network functions
+
+        def Network_packet(self, data):
+            self._server.Broadcast({"action": "packetConfirm", "packet": 123})
+
+class ServerTest(Server):
+        channelClass = ClientChannel
+
+        def __init__(self, *args, **kwargs):
+            Server.__init__(self, *args, **kwargs)
+            self.clients = WeakKeyDictionary() #Store weak references to clients
+            print 'Server launched'
+
+        def Connected(self, channel, addr):
+            self.AddClient(channel)
+
+        def AddClient(self, client):
+            print "New client at " + str(client.addr)
+            self.clients[client] = True
+            print "clients", [c for c in self.clients]
+
+        def DelClient(self, client):
+            print "Deleting " + str(client.addr)
+            del self.clients[client]
+
+        def Broadcast(self, data):
+            [c.Send(data) for c in self.clients]
+
+        def Run(self):
+            while True:
+                    self.Pump()
+                    sleep(0.0001)
+
+# get command line argument of server, port
+if len(sys.argv) != 2:
+	print "Usage:", sys.argv[0], "host:port"
+	print "e.g.", sys.argv[0], "localhost:31425"
+else:
+	host, port = sys.argv[1].split(":")
+	s = ServerTest(localaddr=(host, int(port)))
+	s.Run()
+
+
