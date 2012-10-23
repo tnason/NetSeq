cf4768ffed0e7933a7cba47abb570fad548b0988
Andrew Head <head.andrewm@gmail.com>
2012-10-23 12:31:18 -0400

    Playback buttons and tabs for GUI and documenting GUI tests

--- app/gui.py
+++ app/gui.py
@@ -5,6 +5,9 @@ from audio.music_player import MusicPlayer
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.tabbedpanel import TabbedPanelHeader
+from kivy.uix.floatlayout import FloatLayout
 
 class GUI(Widget):
     """Widget for all user interaction with sequencer"""
@@ -84,20 +87,165 @@ class GUI(Widget):
         PLAYBACK_WIDTH = GRID_WIDTH
         PLAYBACK_HEIGHT = WINDOW_HEIGHT - OUTER_PADDING - GRID_HEIGHT - \
                           OUTER_PADDING - OUTER_PADDING
+        PLAYBACK_CENTER_Y = PLAYBACK_Y + (PLAYBACK_HEIGHT / 2)
 
-        PAGE_BUTTON_WIDTH = 40
-        PAGE_BUTTON_HEIGHT = 40
-        NUM_PAGE_BUTTONS = MusicPlayer.NUM_PAGES
         PLAY_BUTTON_WIDTH = 50
         PLAY_BUTTON_HEIGHT = 50
-        TRACK_BUTTON_WIDTH = 40
-        TRACK_BUTTON_HEIGHT = 40
-        NUM_TRACK_BUTTONS = MusicPlayer.NUM_TRACKS
         PLAYALL_BUTTON_WIDTH = 50
         PLAYALL_BUTTON_HEIGHT = 50
+        PLAYALL_BUTTON_FONT_SIZE = 8
+        PAGE_BUTTON_WIDTH = 25
+        PAGE_BUTTON_HEIGHT = 30
+        NUM_PAGE_BUTTONS = MusicPlayer.NUM_PAGES
+        PAGE_LABEL_WIDTH = (PAGE_BUTTON_WIDTH * NUM_PAGE_BUTTONS)
+        PAGE_LABEL_HEIGHT = 20
+        PAGE_LABEL_FONT_SIZE = 10
+        PAGE_LABEL_OFFSET = 5
+        TRACK_BUTTON_WIDTH = 30
+        TRACK_BUTTON_HEIGHT = 40
+        NUM_TRACK_BUTTONS = MusicPlayer.NUM_TRACKS
+        NUM_PLAYBACK_ELEMENTS = 4
 
-        PLAYBACK_PADDING = PLAYBACK_WIDTH - (PAGE_BUTTON_WIDTH * 
-                           NUM_PAGE_BUTTONS) - (
+        PLAYBACK_PADDING = (PLAYBACK_WIDTH - (PAGE_BUTTON_WIDTH * 
+                            NUM_PAGE_BUTTONS) - (PLAY_BUTTON_WIDTH) - 
+                            (TRACK_BUTTON_WIDTH * NUM_TRACK_BUTTONS) - 
+                            PLAYALL_BUTTON_WIDTH) / (NUM_PLAYBACK_ELEMENTS + 1)
+        
+        # Play/pause button
+        PLAY_BUTTON_X = PLAYBACK_X + PLAYBACK_PADDING
+        play_button = Button(width=PLAY_BUTTON_WIDTH, 
+                             height=PLAY_BUTTON_HEIGHT)
+        play_button.x = PLAY_BUTTON_X
+        play_button.center_y = PLAYBACK_CENTER_Y
+        self.play_button = play_button
+        self.add_widget(play_button)
+
+        # Button to play all pages
+        playall_button = Button(width=PLAYALL_BUTTON_WIDTH,
+                                height=PLAYALL_BUTTON_HEIGHT,
+                                text="Play all", 
+                                text_size=[PLAYALL_BUTTON_WIDTH, 
+                                    PLAYALL_BUTTON_HEIGHT], 
+                                font_size=PLAYALL_BUTTON_FONT_SIZE,
+                                halign='center', valign='middle')
+        playall_button.x = play_button.right + PLAYBACK_PADDING
+        playall_button.center_y = PLAYBACK_CENTER_Y
+        self.playall_button = playall_button
+        self.add_widget(playall_button)
+
+        # Page selection buttons
+        self.page_buttons = []
+        page_buttons = []
+        page_label = Label(text="Page Select", text_size=[PAGE_LABEL_WIDTH, 
+                           PAGE_LABEL_HEIGHT], font_size=PAGE_LABEL_FONT_SIZE,
+                           width=PAGE_LABEL_WIDTH, height=PAGE_LABEL_HEIGHT,
+                           halign='center', valign='middle')
+        page_button_x = playall_button.right + PLAYBACK_PADDING
+        page_label.x = page_button_x
+        page_label.top = PLAYBACK_CENTER_Y - (PAGE_BUTTON_HEIGHT / 2) - \
+                         PAGE_LABEL_OFFSET
+        self.add_widget(page_label)
+        for page in range(0, NUM_PAGE_BUTTONS):
+            page_button = ToggleButton(width=PAGE_BUTTON_WIDTH,
+                                       height=PAGE_BUTTON_HEIGHT)
+            page_button.x = page_button_x
+            page_button.center_y = PLAYBACK_CENTER_Y
+            page_buttons.append(page_button)
+            self.add_widget(page_button)
+            page_button_x += PAGE_BUTTON_WIDTH
+
+        self.page_buttons = page_buttons
+
+        # Track selection buttons
+        track_buttons = []
+        self.track_buttons = []
+        track_button_x = page_buttons[len(page_buttons) - 1].right + \
+                         PLAYBACK_PADDING
+        for track in range(0, NUM_TRACK_BUTTONS):
+            track_button = ToggleButton(width=TRACK_BUTTON_WIDTH, 
+                                        height=TRACK_BUTTON_HEIGHT)
+            track_button.x = track_button_x
+            track_button.center_y = PLAYBACK_CENTER_Y
+            track_buttons.append(track_button)
+            self.add_widget(track_button)
+            track_button_x += TRACK_BUTTON_WIDTH
+        
+        self.track_buttons = track_buttons        
+
+        # SETTINGS TABS
+        TABS_X = OUTER_PADDING + GRID_WIDTH + OUTER_PADDING
+        TABS_Y = GRID_Y
+        TABS_WIDTH = WINDOW_WIDTH - OUTER_PADDING - GRID_WIDTH - \
+                     OUTER_PADDING - OUTER_PADDING
+        TABS_HEIGHT = GRID_HEIGHT
+        # Note: it's a good idea to make these tabs the size of our icons,
+        # which is 48x48
+        TAB_HEADER_WIDTH = 48
+        TAB_HEADER_HEIGHT = TAB_HEADER_WIDTH
+        TAB_HEADER_FONT_SIZE = 20
+        SECTION_LABEL_FONT_SIZE = 10
+        SECTION_LABEL_HEIGHT = 30
+        ELEMENT_LABEL_FONT_SIZE = 8
+        ELEMENT_LABEL_HEIGHT = 20
+        TAB_CONTENT_HEIGHT = TABS_HEIGHT - TAB_HEADER_HEIGHT
+        TAB_CONTENT_TOP = TABS_Y + TAB_CONTENT_HEIGHT
+
+        # Element is button, label, etc. Section is vertical group of elements
+        TAB_SECTION_PADDING = 20
+        TAB_ELEMENT_PADDING = 10
+
+        # Create main tabbed panel
+        tabs = TabbedPanel(tab_width=TAB_HEADER_WIDTH, 
+                           tab_height=TAB_HEADER_HEIGHT, width=TABS_WIDTH, 
+                           height=TABS_HEIGHT)
+        tabs.x = TABS_X
+        tabs.y = TABS_Y
+        self.add_widget(tabs)
+        self.tabs = tabs
+
+        # Music tab (default)
+        # TODO: make these paths absolute?
+        music_tab_content = Widget(width=TABS_WIDTH, height=TAB_CONTENT_HEIGHT)
+        tabs.default_tab_content = music_tab_content
+        tabs.default_tab.text = ""
+        # TODO: replace this image with 48x48
+        tabs.default_tab.background_normal = \
+            "../assets/icons/audio-keyboard.png"
+        tabs.default_tab.background_down = \
+            "../assets/icons/audio-keyboard.png"
+
+        # Global music options
+        global_music_label = Label(text="Global", 
+                                   font_size=SECTION_LABEL_FONT_SIZE,
+                                   width=TABS_WIDTH, 
+                                   height=SECTION_LABEL_HEIGHT,
+                                   valign='middle')
+        global_music_label.center_x = tabs.center_x
+        global_music_label.top = TAB_CONTENT_TOP - TAB_SECTION_PADDING
+        print "Music label top: ", str(global_music_label.top)
+        print "Music label y: ", str(global_music_label.y)
+        print "Tab content height: ", str(TAB_CONTENT_HEIGHT)
+        print "Music content height: ", str(music_tab_content.height)
+        print "Grid height: ", str(GRID_HEIGHT)
+        music_tab_content.add_widget(global_music_label)
+
+        # Network tab
+        network_tab = TabbedPanelHeader()
+        network_tab.text = ""
+        network_tab.background_normal = \
+            "../assets/icons/network-wired.png"
+        network_tab.background_down = \
+            "../assets/icons/network-wired.png"
+        tabs.add_widget(network_tab)
+
+        # System options tab
+        system_tab = TabbedPanelHeader()
+        system_tab.background_normal = \
+            "../assets/icons/computer-4.png"
+        system_tab.background_down = \
+            "../assets/icons/computer-4.png"
+        tabs.add_widget(system_tab)
+
 
     def set_note(self, note):
         """Set a note in note grid to on or off
